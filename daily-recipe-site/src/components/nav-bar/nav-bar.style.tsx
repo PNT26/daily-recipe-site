1ee7c1bc181783390818749e093c8ff9390280f2
phuntsho-norbu-tenzin-26 <phuntsho.tenzin@selise.ch>
2023-09-28 13:13:56 +0600

    chore(nav): change nav bg

--- daily-recipe-site/src/components/nav-bar/nav-bar.style.tsx
+++ daily-recipe-site/src/components/nav-bar/nav-bar.style.tsx
@@ -1,24 +1,22 @@
 import styled from "styled-components";
 
 export const StyledNavBar = styled.div`
-
-    .nav {
-      backdrop-filter: blur(12px);
-      &__dropdown {
-        &-container {
-          width: 206px;
-          right: 0;
-          top: 30px;
-        }
-        //&--button:hover + .nav__dropdown-container {
-        //  display: block;
-        //}
-        &-container--list {
-          transition: 0.2s ease-in;
-          &:hover {
-            background-color: var(--primary-100);
-          }
+  .nav {
+    &__dropdown {
+      &-container {
+        width: 206px;
+        right: 0;
+        top: 30px;
+      }
+      //&--button:hover + .nav__dropdown-container {
+      //  display: block;
+      //}
+      &-container--list {
+        transition: 0.2s ease-in;
+        &:hover {
+          background-color: var(--primary-100);
         }
       }
     }
-`
+  }
+`;
